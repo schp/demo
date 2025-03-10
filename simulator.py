f6a759255627b7f8811a639e2a245d122f5b4463
schp <schonhofen@gmail.com>
2025-03-10 02:13:07 +0100

    improve the simulator

--- simulator.py
+++ simulator.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 
+from multiprocessing import Process
 import random
 import requests
+import time
 
-for index in range(100):
-    requests.post(
-        'http://localhost:8080/api/event',
-        json={
-          'regionId': random.choice(['DE', 'UK']),
-          'eventId': index,
-          'secretToken': 'some-secret',
-          'siteId': random.choice(['SITE-1', 'SITE-2', 'SITE-3']),
-          'deviceId': 'DEVICE-1'
-        })
+def pump_events(base):
+    for index in range(1000):
+        requests.post(
+            'http://localhost:8080/api/event',
+            json={
+              'regionId': random.choice(['DE', 'UK']),
+              'eventId': base * 1000 + index,
+              'secretToken': 'some-secret',
+              'siteId': random.choice(['SITE-1', 'SITE-2', 'SITE-3']),
+              'deviceId': 'DEVICE-1'
+            })
 
 emails = [f'user{index}@acme.com' for index in range(1, 7)]
-for index in range(100):
-    response = requests.post(
-        'http://localhost:8080/api/review',
-        json={'userId': random.choice(emails)})
-    events = response.json()['events']
-    if len(events) != 0:
-        event = random.choice(events)
-        requests.put(
-            f'http://localhost:8080/api/review/{event['regionId']}/{event['eventId']}',
-            json={'review': random.choice(['approve', 'disapprove', 'not-sure'])})
+
+def pump_reviews():
+    for index in range(2000):
+        response = requests.post(
+            'http://localhost:8080/api/review',
+            json={'userId': random.choice(emails)})
+        events = response.json()['events']
+        for event in events:
+            if random.random() < 0.75:
+                requests.put(
+                    f'http://localhost:8080/api/review/{event['regionId']}/{event['eventId']}',
+                    json={'review': random.choice(['approve', 'disapprove', 'not-sure'])})
+        time.sleep(0.01)
+
+event_pumps = [Process(target=pump_events, args=(index, )) for index in range(3)]
+review_pumps = [Process(target=pump_reviews) for index in range(5)]
+for process in event_pumps + review_pumps:
+    process.start()
+for process in event_pumps + review_pumps:
+    process.join()
